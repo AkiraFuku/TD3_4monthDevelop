f7c4107f9b9e84b43fd4aef6f341136a71bc50ab
Nyaaa0778 <k024g0093@g.neec.ac.jp>
2026-03-09 16:05:59 +0900

    移動処理だけ追加

--- project/application/scene/GameScene.h
+++ project/application/scene/GameScene.h
@@ -1,38 +1,42 @@
 #pragma once
-#include"MathFunction.h"
-#include "Sprite.h"
-#include"Object3D.h"
-#include "Model.h"
-#include "Camera.h"
-#include "ParicleEmitter.h"
 #include "Audio.h"
-#include "TextureManager.h"
-#include "Scene.h"
-#include <memory>
-#include "Player.h"
-#include "Terrain.h"
+#include "Camera.h"
 #include "DebugCamera.h"
+#include "MathFunction.h"
+#include "Model.h"
+#include "Object3D.h"
+#include "ParicleEmitter.h"
+#include "Player.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "Terrain.h"
+#include "TextureManager.h"
+#include <memory>
 
-class GameScene :public Scene
-{
+class GameScene : public Scene {
 public:
-    void Initialize() override;
-    void Finalize()override;
-    void Update()override;
-    void Draw()override;
+  void Initialize() override;
+  void Finalize() override;
+  void Update() override;
+  void Draw() override;
+
 private:
-    std::unique_ptr<Camera> camera;
-    std::unique_ptr<Sprite> sprite;
-    std::unique_ptr<Object3d> object3d2;
-    std::unique_ptr<Object3d> object3d;
-    std::unique_ptr<ParicleEmitter> emitter;
-  uint32_t handle_=0;
+  std::unique_ptr<Camera> camera;
+  std::unique_ptr<Sprite> sprite;
+  std::unique_ptr<Object3d> object3d2;
+  std::unique_ptr<Object3d> object3d;
+  std::unique_ptr<ParicleEmitter> emitter;
+  uint32_t handle_ = 0;
 
   DebugCamera debugCamera_;
   bool isDebugCamera_ = false;
 
-  Player* player_;
-  Terrain* terrain_;
+  /*Player* player_;
+  Terrain* terrain_;*/
 
+  // ----- Player -----
+  std::unique_ptr<Player> player_;
+  
+  //プレイヤーの位置
+  Vector3 playerPos_ = {-10.0f,0.0f, 0.0f};
 };
-
